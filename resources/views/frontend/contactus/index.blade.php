b7893a641fbb604a3d521f57f66e9f8cefb25aed
Sadekkh <khmirisadok21@gmail.com>
2023-06-05 14:33:48 +0100

    last commit' q
    é

--- resources/views/frontend/contactus/index.blade.php
+++ resources/views/frontend/contactus/index.blade.php
@@ -5,23 +5,31 @@
 @section('content')
 
 
-<html>
-<head>
-<style>
-h1 {text-align: center;}
-p {text-align: center;}
-div {text-align: center;}
-</style>
-</head>
-<body>
+    <html>
 
-<h1>Hello</h1>
-<p>For any feedback or suggestions, you can contact us by this Gmail:</p>
-<p>bassmaelwazzan@gmail.com</p>
+    <head>
+        <style>
+            h1 {
+                text-align: center;
+            }
+
+            p {
+                text-align: center;
+            }
+
+            div {
+                text-align: center;
+            }
+        </style>
+    </head>
+
+    <body>
 
 
-</body>
-</html>
+
+    </body>
+
+    </html>
 
 
 @endsection
